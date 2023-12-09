16fc5f86a154abb94981449af3cd4be3a73a8a10
evisp <evisplaku@gmail.com>
2023-12-09 16:37:06 +0100

    add: section on index.html to search for movie along with back end route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from models import db
 from models.film_model import Film
 from models.actor_model import Actor
@@ -16,13 +16,34 @@ db.init_app(app)
 from models.film_model import Film
 from models.actor_model import Actor
 
-
+"""
 @app.route('/')
 def index():
     trending_movies = Film.query.order_by(func.rand()).limit(10).all()    
     return render_template('index.html', trending_movies=trending_movies)
+"""
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    trending_movies = Film.query.limit(10).all()
+    search_term = ""  # Default value for search_term
 
+    if request.method == 'POST':
+        search_term = request.form.get('search_term', '')
+        # Query films that contain the search term in the title
+        search_results = Film.query.filter(Film.title.ilike(f"%{search_term}%")).all()
+    else:
+        search_results = []
 
+    return render_template('index.html', trending_movies=trending_movies, search_results=search_results, search_term=search_term)
+
+@app.route('/search_movies', methods=['POST'])
+def search_movies():
+    search_term = request.form.get('search_term', '')
+    # Perform a search query based on the search_term
+    # You can use Film.query.filter(Film.title.ilike(f"%{search_term}%")).all()
+    # Update the variable 'search_results' accordingly
+    search_results = [...]  # Replace with your actual search query
+    return render_template('search_results.html', search_results=search_results)
 
 @app.route('/movies', methods=['GET', 'POST'])
 def movies():
